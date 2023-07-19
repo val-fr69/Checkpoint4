dbca47e81d14359a9aa74f53f1e6dcced5f35c92
val-fr69 <valentin.inacio@live.fr>
2023-07-19 18:49:36 +0200

    add news_read

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -7,16 +7,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/news', name: 'news')]
 class NewsController extends AbstractController
 {
-    #[Route('/news', name: 'news')]
-    public function showArticle(ArticleRepository $articleRepository): Response
+    #[Route('', name: '')]
+    public function index(ArticleRepository $articleRepository): Response
     {
         $articles = $articleRepository->findAll();
 
         return $this->render(
             'news/index.html.twig',
-            ['article' => $articles,]
+            ['articles' => $articles,]
+        );
+    }
+
+    #[Route('/read/{id<^[0-9]+$>}', name: '_read')]
+    public function showArticle(int $id, ArticleRepository $articleRepository): Response
+    {
+        $read = $articleRepository->find($id);
+
+        return $this->render(
+            'news/read.html.twig',
+            ['read' => $read,]
         );
     }
 }
