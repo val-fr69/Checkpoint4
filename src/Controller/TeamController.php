a1333e867dc0eb734f828302b49590adb7c8c457
val-fr69 <valentin.inacio@live.fr>
2023-07-19 22:03:33 +0200

    add on temp

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TeamRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class TeamController extends AbstractController
 {
     #[Route('', name: '')]
-    public function index(): Response
+    public function index(TeamRepository $teamRepository): Response
     {
-        return $this->render('team/index.html.twig', [
-            'controller_name' => 'TeamController',
-        ]);
+        $teams = $teamRepository->findAll();
+
+        return $this->render(
+            'team/index.html.twig',
+            ['teams' => $teams,]
+        );
     }
 }
