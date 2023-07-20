11b8ddb322a39f61d55e1ff2ca463f3d1891b67c
val-fr69 <valentin.inacio@live.fr>
2023-07-20 16:52:59 +0200

    add update/delete

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -8,12 +8,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Players;
 use App\Form\PlayerType;
+use App\Repository\PlayersRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 class PlayerController extends AbstractController
 {
+    #[Route('/player', name: 'list_player')]
+    public function index(PlayersRepository $playersRepository): Response
+    {
+        $players = $playersRepository->findAll();
+
+        return $this->render(
+            'player/list.html.twig',
+            ['players' => $players,]
+        );
+    }
+
+
     #[Route('/player/new', name: 'new_player', methods: ['GET', 'POST'])]
-    public function index(
+    public function new(
         Request $request,
         EntityManagerInterface $manager
     ): Response {
@@ -29,12 +42,61 @@ class PlayerController extends AbstractController
 
             $this->addFlash(
                 'success',
-                'Votre joueur a bien été ajouté à une équipe'
+                'Le joueur a bien été ajouté à une équipe'
             );
         }
 
         return $this->render('player/new.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    #[Route('/player/edit/{id}', name: 'edit_player', methods: ['GET', 'POST'])]
+    public function edit(
+        int $id,
+        EntityManagerInterface $manager,
+        PlayersRepository $playersRepository,
+        Request $request
+    ): Response {
+
+        $player = $playersRepository->find($id);
+
+        $form = $this->createForm(PlayerType::class, $player);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $player = $form->getData();
+
+            $manager->persist($player);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Le joueur a bien été modifié'
+            );
+        }
+
+        return $this->render('player/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/player/delete/{id}', name: 'delete_player', methods: ['GET'])]
+    public function delete(
+        int $id,
+        EntityManagerInterface $manager,
+        PlayersRepository $playersRepository
+    ): Response {
+
+        $player = $playersRepository->find($id);
+        $manager->remove($player);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            'Le joueur a bien été supprimé'
+        );
+
+        return $this->redirectToRoute('list_player');
     }
 }
